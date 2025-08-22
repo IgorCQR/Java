6269fd58c27d1e4ffd4deecb66c5a70f3ae8a288
Ashey <Igorregatieri9@gmail.com>
2025-08-22 00:33:54 -0300

    atividade
    conclusao da tarefa sobre operadores logicos e aritmericos do Neto

--- src/Operadores.java
+++ src/Operadores.java
@@ -3,21 +3,60 @@ import java.util.Scanner;
 public class Operadores {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        float numero1 = input.nextInt();
-        float numero2 = input.nextInt();
+
+        System.out.print("Digite um numero: "); float numero1 = input.nextInt();
+        System.out.print("Digite outro numero: "); float numero2 = input.nextInt();
 
         float soma = numero1 + numero2;
         float diferenca = numero1 - numero2;
         float produto = numero1 * numero2;
         float divisao = numero1 / numero2;
         float resto = numero1 % numero2;
-        float inteiro = numero1 + numero2;
 
-        System.out.println("A soma entre " + numero1 + " e " + numero2 + ": " + soma);
+        System.out.println("\nOperadores aritméticos:");
+        System.out.println("\nA soma entre " + numero1 + " e " + numero2 + ": " + soma);
         System.out.println("A diferença entre " + numero1 + " e " + numero2 + ": " + diferenca);
         System.out.println("O produto entre " + numero1 + " e " + numero2 + ": " + produto);
         System.out.println("A divisão entre " + numero1 + " e " + numero2 + ": " + divisao);
         System.out.println("O resto da divisão entre " + numero1 + " e " + numero2 + ": " + resto);
+
+
+        boolean resultado;
+        System.out.println("\nOperadores lógicos: ");
+        System.out.println("\nTestando o operador lógico E (&&):");
+        resultado = (numero1 > numero2) && (numero1 < numero2);
+        System.out.println("(" + numero1 + " > " + numero2 + ") && (" + numero1 + " < " + numero2 + ") = " + resultado);
+
+        resultado = (numero1 < numero2) && (numero1 > numero2);
+        System.out.println("(" + numero1 + " < " + numero2 + ") && (" + numero1 + " > " + numero2 + ") = " + resultado);
+
+        resultado = (numero1 < numero2) && (numero1 < numero2);
+        System.out.println("(" + numero1 + " < " + numero2 + ") && (" + numero1 + " < " + numero2 + ") = " + resultado);
+
+        System.out.println("\nTestando o operador lógico OU (||):");
+        resultado = (numero1 > numero2) || (numero1 < numero2);
+        System.out.println("(" + numero1 + " > " + numero2 + ") || (" + numero1 + " < " + numero2 + ") = " + resultado);
+
+        resultado = (numero1 > numero2) || (numero1 > numero2);
+        System.out.println("(" + numero1 + " > " + numero2 + ") || (" + numero1 + " > " + numero2 + ") = " + resultado);
+
+        resultado = (numero1 < numero2) || (numero1 < numero2);
+        System.out.println("(" + numero1 + " < " + numero2 + ") || (" + numero1 + " < " + numero2 + ") = " + resultado);
+
+        System.out.println("\nTestando o operador lógico NÃO (!) para A (Primeira proposição):");
+        resultado = !(numero1 > numero2);
+        System.out.println("! (" + numero1 + " > " + numero2 + ") = " + resultado);
+
+        resultado = !(numero1 < numero2);
+        System.out.println("! (" + numero1 + " < " + numero2 + ") = " + resultado);
+
+        System.out.println("\nTestando o operador lógico NÃO (!) para B (Segunda proposição):");
+        resultado = !(numero1 > numero2);
+        System.out.println("! (" + numero1 + " > " + numero2 + ") = " + resultado);
+
+        resultado = !(numero1 < numero2);
+        System.out.println("! (" + numero1 + " < " + numero2 + ") = " + resultado);
+
         input.close();
     }
 }
---
+++ src/Switch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; //copie retirando o Scanner
+public class Switch {
+    public static void main(String[] args) {
+        System.out.print("Digite um numero: ");
+        Scanner input = new Scanner(System.in);
+        int numero = input.nextInt();
+        switch (numero) {
+            case 0:
+                System.out.println("Dado = 0");
+                break;
+            case 1:
+                System.out.println("Dado = 1");
+                break;
+        }
+        System.out.println("Decisão com IF/ELSE");
+        if(numero == 0){
+            System.out.println("Dado informado = 0");
+        }else if(numero == 1){
+            System.out.println("Dado informado = 1");
+        }else{
+            System.out.println("Dado informado" + numero);
+        }
+    }
+}
